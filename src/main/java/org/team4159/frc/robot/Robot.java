cb8da2ddada389752ec5fedec3b29937296cf0e1
Rishi Nath <rishi.nath@gmail.com>
2020-01-13 18:54:33 -0800

    refactor a bit

--- src/main/java/org/team4159/frc/robot/Robot.java
+++ src/main/java/org/team4159/frc/robot/Robot.java
@@ -1,18 +1,10 @@
 package org.team4159.frc.robot;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import org.team4159.frc.robot.subsystems.Drivetrain;
-import org.team4159.frc.robot.traj.Trajectories;
 
 public class Robot extends TimedRobot {
   private RobotContainer robot_container;
--- src/main/java/org/team4159/frc/robot/RobotContainer.java
+++ src/main/java/org/team4159/frc/robot/RobotContainer.java
@@ -4,13 +4,11 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
 import org.team4159.frc.robot.commands.CharacterizationRoutine;
 import org.team4159.frc.robot.commands.FollowTrajectory;
 import org.team4159.frc.robot.subsystems.Drivetrain;
-import org.team4159.frc.robot.traj.Trajectories;
 
 import static org.team4159.frc.robot.Constants.*;
 
@@ -21,16 +19,16 @@ public class RobotContainer {
   private final Joystick right_joy = new Joystick(CONTROLS.RIGHT_JOY);
 
   private final FollowTrajectory follow_trajectory_command = new FollowTrajectory(
-          Trajectories.testTrajectory(),
+          TRAJECTORIES.TEST_TRAJECTORY,
           drivetrain,
           drivetrain::getPose,
           drivetrain::getWheelSpeeds,
           drivetrain::voltsDrive);
   private final CharacterizationRoutine characterization_command = new CharacterizationRoutine(
           drivetrain);
+  private final InstantCommand default_command = new InstantCommand();
 
   private SendableChooser<Command> autonomous_chooser;
-
 
   public RobotContainer() {
     drivetrain.setDefaultCommand(
@@ -42,7 +40,7 @@ public class RobotContainer {
     autonomous_chooser = new SendableChooser<>();
     autonomous_chooser.addOption("Follow Trajectory", follow_trajectory_command);
     autonomous_chooser.addOption("Characterization Routine", characterization_command);
-    autonomous_chooser.addOption("No Autonomous", new InstantCommand());
+    autonomous_chooser.setDefaultOption("No Autonomous", default_command);
   }
 
   public Command getAutonomousCommand() {

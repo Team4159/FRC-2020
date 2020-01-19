6a3d09de0795d93e77f3d86c4039e10bc2fb90ba
Rishi Nath <rishi.nath@gmail.com>
2020-01-18 20:31:53 -0800

    tests

--- src/test/java/OdometryTest.java
+++ src/test/java/OdometryTest.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.team4159.lib.CsvWriter;
+import org.team4159.lib.Odometry;
 
 public class OdometryTest {
+  private final CsvWriter csv_writer = new CsvWriter("odometry_test");
+
+  private Odometry odometry;
+
+  @Before
+  public void reset() {
+    odometry = new Odometry();
+  }
+
+  @Test
+  public void GoesForwardOneMeter() {
+    for (int i = 0; i < 100; i++) {
+      odometry.update(0.01, 0.0);
+
+      final Pose2d current_pose = odometry.getPose();
+
+      csv_writer.write(current_pose.getTranslation().getX(),
+                       current_pose.getTranslation().getY(),
+                       current_pose.getRotation().getDegrees());
+    }
+
+    csv_writer.finish();
+
+    final Pose2d final_pose = odometry.getPose();
+
+    final double final_x = final_pose.getTranslation().getX();
+    final double final_y = final_pose.getTranslation().getY();
+    final double final_direction = final_pose.getRotation().getDegrees();
+
+    Assert.assertEquals("Expected Final X: 1.0, Output Final X: " + final_x, 1.0, final_x, 0.0);
+    Assert.assertEquals("Expected Final Y: 0.0, Output Final Y: " + final_y, 0.0, final_y, 0.0);
+    Assert.assertEquals("Expected Final Direction: 0.0, Output Final Direction: " + final_direction, 0.0, final_direction, 0.0);
+  }
 }
--- src/test/java/TrajectoryTest.java
+++ src/test/java/TrajectoryTest.java
@@ -1,16 +1,39 @@
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import org.team4159.frc.robot.Trajectories;
+import org.team4159.lib.CsvWriter;
 
-public class TrajectoryPrinter {
-  private Trajectory trajectory = Trajectories.TEST_TRAJECTORY;
+import java.util.List;
+
+public class TrajectoryTest {
+  private final CsvWriter csv_writer = new CsvWriter("trajectory_test");
+
+  private final Trajectory test_trajectory = Trajectories.TEST_TRAJECTORY;
 
   @Test
-  public void PrintTrajectory() {
-    for (Trajectory.State state : trajectory.getStates()) {
-      System.out.println(state.poseMeters.getTranslation().getX() + ", " + state.poseMeters.getTranslation().getY());
+  public void TestTrajectory() {
+    final List<Trajectory.State> states = test_trajectory.getStates();
+
+    for (Trajectory.State state : states) {
+      csv_writer.write(state.poseMeters.getTranslation().getX(),
+                       state.poseMeters.getTranslation().getY(),
+                       state.poseMeters.getRotation().getDegrees());
     }
+
+    csv_writer.finish();
+
+    final Pose2d final_pose = states.get(states.size() - 1).poseMeters;
+
+    final double final_x = final_pose.getTranslation().getX();
+    final double final_y = final_pose.getTranslation().getY();
+    final double final_direction = final_pose.getRotation().getDegrees();
+
+    Assert.assertEquals("Expected Final X: 0.0, Output Final X: " + final_x, 0.0, final_x, 0.1);
+    Assert.assertEquals("Expected Final Y: 3.0, Output Final Y: " + final_y, 3.0, final_y, 0.1);
+    Assert.assertEquals("Expected Final Direction: 90.0, Output Final Direction: " + final_direction, 90.0, final_direction, 1.0);
   }
 }

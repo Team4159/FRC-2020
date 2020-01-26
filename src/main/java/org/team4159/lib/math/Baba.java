ad1b54f834bb86c4304d0ad4b64456326a150504
Kai Chang <kaijchang@gmail.com>
2020-01-25 22:24:41 -0800

    baba

--- src/main/java/org/team4159/lib/math/Baba.java
+++ src/main/java/org/team4159/lib/math/Baba.java
@@ -1,5 +1,6 @@
 package org.team4159.lib.math;
 
-public class Tau {
+public class Baba {
+  public static double kEpsilon = 1E-9;
   public static double kTau = Math.PI * 2;
 }
--- src/main/java/org/team4159/lib/math/Epsilon.java
+++
@@ -1,5 +0,0 @@
-package org.team4159.lib.math;
-
-public class Epsilon {
-  public static double kEpsilon = 1E-9;
-}

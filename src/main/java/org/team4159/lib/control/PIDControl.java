4518d564841f4d637dffdf2ada30f4993b631fb3
Rishi Nath <rishi.nath@gmail.com>
2020-02-02 15:35:57 -0800

    better pid control

--- src/main/java/org/team4159/lib/control/PIDControl.java
+++ src/main/java/org/team4159/lib/control/PIDControl.java
@@ -1,26 +1,40 @@
 package org.team4159.lib.control;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+
 public class PIDControl {
   private double kP;
   private double kI;
   private double kD;
+  private double interval;
+  private double max_integral;
 
   private double last_error = 0.0;
-  private double error_sum = 0.0;
+  private double sigma_error = 0.0;
   private double goal = 0.0;
 
   public PIDControl(double kP, double kI, double kD) {
+    this(kP, kI, kD, TimedRobot.kDefaultPeriod, Double.POSITIVE_INFINITY);
+  }
+
+  public PIDControl(double kP, double kI, double kD, double interval) {
+    this(kP, kI, kD, interval, Double.POSITIVE_INFINITY);
+  }
+
+  public PIDControl(double kP, double kI, double kD, double dt, double max_integral) {
     setPID(kP, kI, kD);
+    setInterval(dt);
+    setMaxIntegral(max_integral);
   }
 
   public double calculateOutput(double position) {
     final double error = goal - position;
-    final double delta_error = error - last_error;
-    error_sum += error;
+    final double delta_error = (error - last_error) / interval;
+    sigma_error += error;
 
     final double output =
       kP * error +
-      kI * error_sum +
+      kI * sigma_error +
       kD * delta_error;
 
     last_error = error;
@@ -37,9 +51,9 @@ public class PIDControl {
   }
 
   public void setPID(double kP, double kI, double kD) {
-    this.kP = kP;
-    this.kI = kI;
-    this.kD = kD;
+    setP(kP);
+    setI(kI);
+    setD(kD);
   }
 
   public void setP(double kP) {
@@ -54,8 +68,16 @@ public class PIDControl {
     this.kD = kD;
   }
 
+  public void setInterval(double interval) {
+    this.interval = interval;
+  }
+
+  public void setMaxIntegral(double max_integral) {
+    this.max_integral = max_integral;
+  }
+
   public void resetError() {
     last_error = 0.0;
-    error_sum = 0.0;
+    sigma_error = 0.0;
   }
 }

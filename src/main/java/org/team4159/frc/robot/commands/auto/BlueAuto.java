3f1e3a05e23f02b29380039f1c0b4d8f572e88af
Rishi Nath <rishi.nath@gmail.com>
2020-02-16 10:14:25 -0800

    example auto

--- src/main/java/org/team4159/frc/robot/commands/auto/BlueAuto.java
+++ src/main/java/org/team4159/frc/robot/commands/auto/BlueAuto.java
@@ -1,19 +1,32 @@
 package org.team4159.frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-import org.team4159.frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import org.team4159.frc.robot.Trajectories;
+import org.team4159.frc.robot.commands.arm.ToggleArm;
+import org.team4159.frc.robot.commands.drivetrain.TurnDegrees;
+import org.team4159.frc.robot.commands.intake.RunIntake;
+import org.team4159.frc.robot.commands.turret.LimelightSeek;
+import org.team4159.frc.robot.subsystems.*;
 
 /*
  * Placeholder until we plan out an autonomous routine
  */
 public class BlueAuto extends SequentialCommandGroup {
-  public BlueAuto(Drivetrain drivetrain) {
+  public BlueAuto(Drivetrain drivetrain, Arm arm, Intake intake, Turret turret, Shooter shooter) {
     addRequirements(drivetrain);
 
     addCommands(
-      // start position to trench run
-      // shoot ball
+      new ParallelCommandGroup(
+        new FollowTrajectory(Trajectories.SCRIMMAGE_AUTO, drivetrain),
+        new WaitCommand(1).andThen(new ToggleArm(arm).andThen(new RunIntake(5, intake)))
+      ),
+      new TurnDegrees(180, drivetrain),
+      new LimelightSeek(turret),
+      new RunCommand(() -> shooter.setRawSpeed(1), shooter)
     );
   }
 }
